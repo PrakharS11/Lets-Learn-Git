ec2fdd9e3522b9dc0c7454092940f4f91fbb3d82
Prakhar Srivastava <prakharsahai21@gmail.com>
2021-06-04 21:30:53 +0530

    added message in Diff.java

---
+++ Diff.java
@@ -0,0 +1,7 @@
+public class Diff {
+    public static void main(String [] args){
+            int a=10;
+            int b=9;
+            System.out.print("The Diffrence is "+(a-b));        
+    }
+}
